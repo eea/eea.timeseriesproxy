be33a57234788e702422644d1286c2cbfcb0f91a
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-07-01 11:32:14 +0300

    Removed temp folder

--- eea/timeseriesproxy/www/temp/delTempFiles.sh
+++
@@ -1,6 +0,0 @@
-cd /var/www/html/temp
-find . -name '*.pid' -type f -mmin +1440 -delete
-find . -name '*.pdf' -type f -mmin +1440 -delete
-find . -name '*.png' -type f -mmin +1440 -delete
-find . -name '*.csv' -type f -mmin +1440 -delete
-find . -name '*.tif' -type f -mmin +1440 -delete
